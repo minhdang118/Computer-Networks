dc246c33846c56c3db11c6fcc0e2eb691996bf1a
minhdang118 <mdang2@u.rochester.edu>
2023-12-07 00:30:36 -0500

    add rsa

---
+++ secret-handshake/rsa.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "bigint.cpp"
+
+using namespace std;
+
+/* Encryption Function */
+BigInt encrypt(BigInt m, BigInt e, BigInt n){
+    BigInt c;
+    
+    c = operator^(m, e);
+    c = operator%=(c, n);
+    return c;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* Program greetings */
+    cout << "Welcome to the RSA Encryption Program!\n";
+
+    /* Prompt for user input */
+    unsigned long long n;
+    unsigned long long e;
+    unsigned long long d;
+    unsigned long long m;
+    cout << "Enter your number n: ";
+    cin >> n;
+    cout << "Enter your number e: ";
+    cin >> e;
+    cout << "Enter your number d: ";
+    cin >> d;
+    cout << "Enter your message m: ";
+    cin >> m;
+
+    /* Convert to BigInt */
+    BigInt n_bigint(n);
+    BigInt e_bigint(e);
+    BigInt d_bigint(d);
+    BigInt m_bigint(m);
+
+
+
+
+    return 0;
+}
