b0a879614b649d7f482bf991774b95cffbd60a50
minhdang118 <mdang2@u.rochester.edu>
2023-12-13 22:03:35 -0500

    update rsa

--- secret-handshake/rsa.cpp
+++ secret-handshake/rsa.cpp
@@ -4,41 +4,71 @@
 using namespace std;
 
 /* Encryption Function */
-BigInt encrypt(BigInt m, BigInt e, BigInt n){
+BigInt &encrypt(BigInt m, BigInt e, BigInt n){
     BigInt c;
-    if (operator==(operator%(e, BigInt("2")), BigInt("0")))
+    BigInt zero("0");
+    BigInt two("2");
+
+    if (e % two == zero)
     {
-        return operator%(operator^(encrypt(m, operator/(e, BigInt("2")), n), BigInt("2")), n);
-    } 
-    else
-    {
-        return operator*(operator%(m, n), operator%(m, n));
+        /* code */
     }
     
     
-    c = operator^(m, e);
-    c = operator%(c, n);
-    return c;
+    return operator%=(operator^=(m, e), n);
 }
 
+/* Decryption Function */
+BigInt &decrypt(BigInt c, BigInt d, BigInt n){
+    return operator%=(operator^=(c, d), n);
+}
+
+/* Check if input string is number */
+bool is_number(string s){
+    for (int i = 0; i < s.length(); i++){
+        if (!isdigit(s[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+/* Main Function */
 int main(int argc, char const *argv[])
 {
     /* Program greetings */
-    cout << "Welcome to the RSA Encryption Program!\n";
+    cout << "Welcome to the RSA Encryption and Decryption Program!\n\n";
 
     /* Prompt for user input */
-    unsigned long long n;
-    unsigned long long e;
-    unsigned long long d;
-    unsigned long long m;
-    cout << "Enter your number n: ";
-    cin >> n;
-    cout << "Enter your number e: ";
-    cin >> e;
-    cout << "Enter your number d: ";
-    cin >> d;
-    cout << "Enter your message m: ";
-    cin >> m;
+    string n;
+    string e;
+    string d;
+    string m;
+
+    /* Check if input is number */
+    do
+    {
+        cout << "Enter your number n: ";
+        cin >> n;
+    } while (is_number(n) == false);
+    
+    do
+    {
+        cout << "Enter your number e: ";
+        cin >> e;
+    } while (is_number(e) == false);
+
+    do
+    {
+        cout << "Enter your number d: ";
+        cin >> d;
+    } while (is_number(d) == false);
+
+    do
+    {
+        cout << "Enter your number m: ";
+        cin >> m;
+    } while (is_number(m) == false);
 
     /* Convert to BigInt */
     BigInt n_bigint(n);
@@ -46,8 +76,18 @@ int main(int argc, char const *argv[])
     BigInt d_bigint(d);
     BigInt m_bigint(m);
 
+    /* Encrypt */
+    BigInt c = encrypt(m_bigint, e_bigint, n_bigint);
 
+    /* Decrypt */
+    BigInt m2 = decrypt(c, d_bigint, n_bigint);
 
+    /* Print results */
+    cout << "\nYour encrypted message is: ";
+    cout << c << "\n";
+
+    cout << "\nYour decrypted message is: ";
+    cout << m2 << "\n";
 
     return 0;
 }
